e54eb09d83d5b86c0739b3776eebcb9dedd0408e
Pavan Kumar Jadda <17564080+pavankjadda@users.noreply.github.com>
2024-11-06 13:03:08 -0500

    feat(core): first commit

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,27 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import * as React from 'react';
+import { DocumentContext, DocumentProps, Head, Html, Main, NextScript } from 'next/document';
+import { documentGetInitialProps, DocumentHeadTags, DocumentHeadTagsProps } from '@mui/material-nextjs/v14-pagesRouter';
+import theme, { roboto } from '../theme';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body className="antialiased">
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+export default function MyDocument(props: DocumentProps & DocumentHeadTagsProps) {
+	return (
+		<Html lang="en" className={roboto.className}>
+			<Head>
+				{/* PWA primary color */}
+				<meta name="theme-color" content={theme.palette.primary.main} />
+				<link rel="shortcut icon" href="/favicon.ico" />
+				<meta name="emotion-insertion-point" content="" />
+				<DocumentHeadTags {...props} />
+			</Head>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
 }
+
+MyDocument.getInitialProps = async (ctx: DocumentContext) => {
+	const finalProps = await documentGetInitialProps(ctx);
+	return finalProps;
+};
